fb3449932bdafd9135b6efc9c8d743ba4309da1a
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-04 00:32:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,26 @@ def using_shift(array)
   array.shift(2)
 end
 
-def using_concat(array)
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
+def using_insert(array)
+  
+end
+
+def using_uniq(array)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def using_delete_at(array, integer)
   
 end
